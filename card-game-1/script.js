874f6cbfb0f5616d596655b5c6ddc59e6c51852f
Pedro Henrique Zanotelli de Freitas <127994920+Pedro-Zanotelli@users.noreply.github.com>
2024-09-18 23:36:49 -0300

    feat: added the displayCards and shuffleArray function, adds the number of cards selected by the player automatically to the screen and shuffles the cards respectively. fix: the while loop was repeating itself infinitely in the code editor and consequently freezing absolutely everything because it did not have a control parameter, so a limited number of attempts were added to the loop.

--- card-game-1/script.js
+++ card-game-1/script.js
@@ -1,5 +1,42 @@
-const cardList = document.querySelectorAll(".card")
-cardList = []
+let numberOfCards
+()
+
+function getValidNumberOfCards() {
+    
+    while (true) {
+        numberOfCards = Number(prompt("Enter a number of cards between 4 and 14 (inclusive), and it must be even."));
+
+        if (numberOfCards >= 4 && numberOfCards <= 14 && numberOfCards % 2 === 0) {
+            return numberOfCards; 
+        } else {
+            console.log("Invalid input. Please enter an even number between 4 and 14.");
+        }
+    }
+}
+
+let counter = 0
+const addedCardsArray = [
+    "Adicione uma tarefa no botão acima",
+    "Passe o mouse na tarefa para ver o botão excluir",
+    "Clique na tarefa para marca-la como feita"
+];
+
+function addCards() {
+    const addedCard = document.querySelector(".card");
+
+    for(let i = 0; i < numberOfCards; i++ ) {
+        const cardHtml = `
+            <div class="front-face face">
+                 <img src="assets/back.png">
+            </div>
+            <div class="back-face face">        
+                <img src="assets/bobrossparrot.gif">
+            </div>
+        `;
+
+        addedCard.innerHTML += cardHtml;
+    }
+}
 
 function cardFlip(card){
     card.classList.add('flipped');
